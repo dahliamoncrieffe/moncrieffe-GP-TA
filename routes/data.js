0b08bc3eab11fc23a8b8213192e5f8361eaebdee
Dahlia Moncrieffe <dahliamoncrieffe@gmail.com>
2019-07-26 10:18:48 -0700

    Commit num2 - Having issues with REST piece to get the Order data. Working through it.

---
+++ routes/data.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+//This file pvar request = require('request'); // require in request
+var initGet = { uri: 'https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json' };
+//var initPost = { uri: 'http://http://linkToApi.com/post' };
+
+var apiCaller = function (url, cb) {
+    //use request to make the external http call to the JSON api
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            cb(body);// Send body/response to callback
+        }
+    })
+};
+// Call the api with a call back
+var apiGet = function (cb) {
+    return apiCaller(initGet.uri, cb);
+};
+
+var apiPost = function (post, cb) {
+    return apiCaller(initGet.uri + post, cb);
+};
+// Export the functions for external access
+module.exports = {
+    apiGet: apiGet,
+    apiPost: apiPost
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var data = require('./data');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    //res.render('index', { title: "Dahlia's Shopping Cart" });
+    //call the api apiGet and create callback function
+    data.apiGet(function (data) {
+        // render to the index.jade and pass the data from api call
+        res.render('index', { result: data, title: "Dahlia's Shopping Cart" });
+    });
 });
+
+
+
 
 module.exports = router;
